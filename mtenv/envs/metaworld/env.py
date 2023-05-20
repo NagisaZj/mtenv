98b4e564289a06a412b5bff3d4891920c45d9609
NagisaZj <1170863106@qq.com>
2023-05-20 14:45:12 +0800

    mine

--- mtenv/envs/metaworld/env.py
+++ mtenv/envs/metaworld/env.py
@@ -11,6 +11,8 @@ from mtenv.envs.metaworld.wrappers.normalized_env import (  # type: ignore[attr-
 )
 from mtenv.envs.shared.wrappers.multienv import MultiEnvWrapper
 
+# import gym
+# from goal_env.mujoco import *
 EnvBuilderType = Callable[[], Env]
 TaskStateType = int
 TaskObsType = int
@@ -51,6 +53,8 @@ def get_list_of_func_to_make_envs(
     should_perform_reward_normalization: bool = True,
     task_name: str = "pick-place-v1",
     num_copies_per_env: int = 1,
+    fix_goal=True,
+    task_idx=None
 ) -> Tuple[List[Any], Dict[str, Any]]:
     """Return a list of functions to construct the MetaWorld environments
     and a mapping of environment ids to tasks.
@@ -83,11 +87,17 @@ def get_list_of_func_to_make_envs(
     """
     if not benchmark:
         if benchmark_name == "MT1":
-            benchmark = metaworld.ML1(task_name)
+            benchmark = metaworld.ML1(task_name,seed=1)
         elif benchmark_name == "MT10":
-            benchmark = metaworld.MT10()
+            benchmark = metaworld.MT10(seed=1)
         elif benchmark_name == "MT50":
-            benchmark = metaworld.MT50()
+            benchmark = metaworld.MT50(seed=1)
+        elif benchmark_name == "ML1":
+            benchmark = metaworld.ML1(task_name,seed=1)
+        elif benchmark_name == "ML10":
+            benchmark = metaworld.ML10(seed=1)
+        elif benchmark_name == "ML50":
+            benchmark = metaworld.ML50(seed=1)
         else:
             raise ValueError(f"benchmark_name={benchmark_name} is not valid.")
 
@@ -99,26 +109,34 @@ def get_list_of_func_to_make_envs(
     def _get_tasks(current_benchmark):
         return current_benchmark.train_tasks
 
-    def _get_env_id_to_task_map() -> EnvIdToTaskMapType:
+    def _get_env_id_to_task_map(task_idx = None) -> EnvIdToTaskMapType:
         env_id_to_task_map: EnvIdToTaskMapType = {}
         current_benchmark = benchmark
         for env_id in env_id_list:
             for name, _ in _get_class_items(current_benchmark):
                 if name == env_id:
-                    task = random.choice(
-                        [
-                            task
-                            for task in _get_tasks(current_benchmark)
-                            if task.env_name == name
-                        ]
-                    )
+                    if task_idx ==None:
+                        task = random.choice(
+                            [
+                                task
+                                for task in _get_tasks(current_benchmark)
+                                if task.env_name == name
+                            ]
+                        )
+                    else:
+                        tasks = [
+                                task
+                                for task in _get_tasks(current_benchmark)
+                                if task.env_name == name
+                            ]
+                        task = tasks[task_idx]
                     env_id_to_task_map[env_id] = task
         return env_id_to_task_map
 
     if env_id_to_task_map is None:
-        env_id_to_task_map: EnvIdToTaskMapType = _get_env_id_to_task_map()  # type: ignore[no-redef]
+        env_id_to_task_map: EnvIdToTaskMapType = _get_env_id_to_task_map(task_idx=task_idx)  # type: ignore[no-redef]
     assert env_id_to_task_map is not None
-
+    print(env_id_to_task_map)
     def get_func_to_make_envs(env_id: str):
         current_benchmark = benchmark
 
@@ -128,8 +146,63 @@ def get_list_of_func_to_make_envs(
                     env = env_cls()
                     task = env_id_to_task_map[env_id]
                     env.set_task(task)
-                    if should_perform_reward_normalization:
-                        env = NormalizedEnvWrapper(env, normalize_reward=True)
+                    env = NormalizedEnvWrapper(env, normalize_reward=should_perform_reward_normalization)
+                    return env
+
+        return _make_env
+
+    def get_func_to_make_deterministic_envs(env_id: str):
+        current_benchmark = benchmark
+
+        def _make_env():
+            for name, env_cls in _get_class_items(current_benchmark):
+                if name == env_id:
+                    env = env_cls()
+                    task = env_id_to_task_map[env_id]
+                    print(task)
+                    env.set_task(task)
+                    env._freeze_rand_vec = True
+                    env = NormalizedEnvWrapper(env, normalize_reward=should_perform_reward_normalization)
+                    return env
+
+        return _make_env
+
+    def get_func_to_make_stochastic_envs(env_id: str):
+        current_benchmark = benchmark
+
+        def _make_env():
+            # print(env_id_to_task_map,env_id,env_id_list)
+            for name, env_cls in _get_class_items(current_benchmark):
+                # print(name)
+                if name == env_id:
+                    env = env_cls()
+                    task = env_id_to_task_map[env_id]
+                    env.set_task(task)
+                    env._freeze_rand_vec = False
+                    env = NormalizedEnvWrapper(env, normalize_reward=should_perform_reward_normalization)
+                    return env
+
+        return _make_env
+
+    def get_func_to_make_extra_envs(env_id: str):
+        current_benchmark = benchmark
+
+        def _make_env():
+            # print(env_id_to_task_map,env_id,env_id_list)
+            for name, env_cls in _get_class_items(current_benchmark):
+                # print(name)
+                if 1:
+                    import goal_env.mujoco
+                    import goal_env
+                    # env = gym.make('AntMaze1Test-v1')
+                    name='AntUp-v2'
+                    name='AntMaze1-v1'
+                    env = gym.make(name)
+                    print(name)
+                    # import dm_control2gym
+                    # env = dm_control2gym.make(domain_name="reacher", task_name='hard')
+                    env=gym.wrappers.RescaleAction(env,-1,1)
+                    env = NormalizedEnvWrapper(env, normalize_reward=should_perform_reward_normalization)
                     return env
 
         return _make_env
@@ -141,11 +214,162 @@ def get_list_of_func_to_make_envs(
         env_id_list = [
             env_id for env_id_sublist in env_id_list for env_id in env_id_sublist
         ]
-
-    funcs_to_make_envs = [get_func_to_make_envs(env_id) for env_id in env_id_list]
-
+    if fix_goal:
+        funcs_to_make_envs = [get_func_to_make_deterministic_envs(env_id) for env_id in env_id_list]
+    else:
+        funcs_to_make_envs = [get_func_to_make_stochastic_envs(env_id) for env_id in env_id_list]
+        print('stochastic')
+    # funcs_to_make_envs = [get_func_to_make_extra_envs(env_id) for env_id in env_id_list]
+    # env = funcs_to_make_envs[0]()
+    # env.reset()
+    # print(env.env._get_pos_goal())
     return funcs_to_make_envs, env_id_to_task_map
 
+
+def get_list_of_func_to_make_envs_ML(
+    benchmark: Optional[metaworld.Benchmark],
+    benchmark_name: str,
+    env_id_to_task_map: Optional[EnvIdToTaskMapType],
+    should_perform_reward_normalization: bool = True,
+    task_name: str = "pick-place-v1",
+    num_copies_per_env: int = 1,
+    fix_goal=True,
+    task_idx=None
+) -> Tuple[List[Any], Dict[str, Any]]:
+    """Return a list of functions to construct the MetaWorld environments
+    and a mapping of environment ids to tasks.
+
+    Args:
+        benchmark (Optional[metaworld.Benchmark]): `benchmark` to create
+            tasks from.
+        benchmark_name (str): name of the `benchmark`. This is used only
+            when the `benchmark` is None.
+        env_id_to_task_map (Optional[EnvIdToTaskMapType]): In MetaWorld,
+            each environment can be associated with multiple tasks. This
+            dict persists the mapping between environment ids and tasks.
+        should_perform_reward_normalization (bool, optional): Defaults to
+            True.
+        task_name (str, optional): In case of MT1, only . Defaults to
+            "pick-place-v1".
+        num_copies_per_env (int, optional): Number of copies to create for
+            each environment. Defaults to 1.
+
+    Raises:
+        ValueError: if `benchmark` is None and `benchmark_name` is not
+            MT1, MT10, or MT50.
+
+    Returns:
+        Tuple[List[Any], Dict[str, Any]]: A tuple of two elements. The
+        first element is a list of functions to construct the MetaWorld
+        environments and the second is a mapping of environment ids
+        to tasks.
+
+    """
+    if not benchmark:
+        if benchmark_name == "MT1":
+            benchmark = metaworld.ML1(task_name,seed=1)
+        elif benchmark_name == "MT10":
+            benchmark = metaworld.MT10(seed=1)
+        elif benchmark_name == "MT50":
+            benchmark = metaworld.MT50(seed=1)
+        elif benchmark_name == "ML1":
+            benchmark = metaworld.ML1(task_name,seed=1)
+        elif benchmark_name == "ML10":
+            benchmark = metaworld.ML10(seed=1)
+        elif benchmark_name == "ML50":
+            benchmark = metaworld.ML50(seed=1)
+        else:
+            raise ValueError(f"benchmark_name={benchmark_name} is not valid.")
+
+    env_id_list = list(benchmark.train_classes.keys())
+
+    def _get_class_items(current_benchmark):
+        return current_benchmark.train_classes.items()
+
+    def _get_tasks(current_benchmark):
+        return current_benchmark.train_tasks
+
+    def _get_env_id_to_task_map(task_idx = None) -> EnvIdToTaskMapType:
+        env_id_to_task_map: EnvIdToTaskMapType = {}
+        current_benchmark = benchmark
+        for env_id in env_id_list:
+            for name, _ in _get_class_items(current_benchmark):
+                if name == env_id:
+                    if task_idx ==None:
+                        task = random.choice(
+                            [
+                                task
+                                for task in _get_tasks(current_benchmark)
+                                if task.env_name == name
+                            ]
+                        )
+                    else:
+                        tasks = [
+                                task
+                                for task in _get_tasks(current_benchmark)
+                                if task.env_name == name
+                            ]
+                        task = tasks[task_idx]
+                    env_id_to_task_map[env_id] = task
+        return env_id_to_task_map
+
+    if env_id_to_task_map is None:
+        env_id_to_task_map: EnvIdToTaskMapType = _get_env_id_to_task_map(task_idx=None)  # type: ignore[no-redef]
+
+    tasks_list = benchmark.train_tasks
+    assert env_id_to_task_map is not None
+    print(env_id_to_task_map)
+
+
+    def get_func_to_make_deterministic_envs(env_id,index):
+        current_benchmark = benchmark
+
+        def _make_env():
+            for name, env_cls in _get_class_items(current_benchmark):
+                if name == env_id:
+                    env = env_cls()
+                    task = tasks_list[index]
+                    print(task)
+                    env.set_task(task)
+                    env._freeze_rand_vec = True
+                    env = NormalizedEnvWrapper(env, normalize_reward=should_perform_reward_normalization)
+                    return env
+
+        return _make_env
+
+    def get_func_to_make_stochastic_envs(env_id: str):
+        current_benchmark = benchmark
+
+        def _make_env():
+            # print(env_id_to_task_map,env_id,env_id_list)
+            for name, env_cls in _get_class_items(current_benchmark):
+                # print(name)
+                if name == env_id:
+                    env = env_cls()
+                    task = tasks_list[index]
+                    env.set_task(task)
+                    env._freeze_rand_vec = False
+                    env = NormalizedEnvWrapper(env, normalize_reward=should_perform_reward_normalization)
+                    return env
+
+        return _make_env
+
+    if num_copies_per_env > 1:
+        env_id_list = [
+            [env_id for _ in range(num_copies_per_env)] for env_id in env_id_list
+        ]
+        env_id_list = [
+            env_id for env_id_sublist in env_id_list for env_id in env_id_sublist
+        ]
+    print(task_idx)
+    if fix_goal:
+        funcs_to_make_envs = [get_func_to_make_deterministic_envs(env_id_list[0],idx) for idx in task_idx]
+    else:
+        funcs_to_make_envs = [get_func_to_make_stochastic_envs(env_id_list[0],idx) for idx in task_idx]
+    # env = funcs_to_make_envs[0]()
+    # env.reset()
+    # print(env.env._get_pos_goal())
+    return funcs_to_make_envs, env_id_to_task_map
 
 def build(
     benchmark: Optional[metaworld.Benchmark],
